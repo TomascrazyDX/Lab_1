b0c40c713f27f82ad8bfa20f0ac85c956ef80c6b
TomascrazyDX <tomasarango22@gmail.com>
2025-05-08 16:41:01 -0400

    changed the version of the unity i was, added and finished the boid simulation code and added  some stuff in the boid code
    the boid code needs some fixes tho

--- Assets/Boid.cs
+++ Assets/Boid.cs
@@ -1,5 +1,20 @@
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Boid : MonoBehaviour
 {
+    public Rigidbody rigidbody;
+    private void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        rigidbody.linearVelocity = Random.insideUnitSphere;
+    }
+    public void Update()
+    {
+        AlignToVelocity();
+    }
+    public void AlignToVelocity()
+    {
+    //transform.forward = new Vector3.RotateTowards(transform.forward, rigidbody.linearVelocity.normalized, Mathf.Deg2Rad * 1800 *  Time.deltaTime * 100);
+    }
 }
--- Assets/BoidSimulation.cs
+++ Assets/BoidSimulation.cs
@@ -1,8 +1,13 @@
+using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class BoidSimulationControl : MonoBehaviour
 {
+    public GameObject boidPrefab = null;
+    public int BoidsToSpawn = 10;
+
+
     // to select and switch what mouse buttons do for Assignment1
     public enum ControlMode
     {
@@ -13,42 +18,47 @@ public class BoidSimulationControl : MonoBehaviour
     }
     //which mode are we in  (is selected with a mouse button)
     public ControlMode controlMode = ControlMode.seek;
-    public int GameObject boidPrefab = null;
 
-    public int numBoidsToSpawn = 10;
-
-
+    public List<Boid> boids = null;
 
     private void Start()
     {
-        Vector3 position = new Vector3(Random.Range(-1.4f, 01.4f ),  Random.Range(-0f, 1.4f), Random.Range(-0.9f, 0.9f));
-        Instantiate(boidPrefab, position, rotation);
+        Vector3 position = new Vector3(Random.Range(-1.4f, 1.4f), Random.Range(0f, 1.4f), Random.Range(-0.9f, 0.9f));
+        Quaternion rotation = Random.rotation;
+
+        for (int i = 0; i <= BoidsToSpawn; i++)
+        {
+            GameObject newBoid = Instantiate(boidPrefab, new Vector3(Random.Range(-0.7f, 0.7f), Random.Range(0, 0.7f), Random.Range(-0.4f, 0.4f)), Random.rotation);
+            boids.Add(newBoid.GetComponent<Boid>());
+        }
+
+        //spawn the boids
+        GameObject spawnedBoid = Instantiate(boidPrefab, position, rotation);
+
+        spawnedBoid.transform.localScale *= Random.Range(0.9f, 3f);
     }
-
-    public int controlModeInt = 0;
-
     private void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Alpha1))
+        if (Input.GetKeyDown(KeyCode.Alpha1))
         {
             controlMode = ControlMode.seek;
-            controlModeInt = 0;
         }
         if (Input.GetKeyDown(KeyCode.Alpha2))
         {
             controlMode = ControlMode.pursue;
-            controlModeInt = 1;
         }
         if (Input.GetKeyDown(KeyCode.Alpha3))
         {
             controlMode = ControlMode.food;
-            controlModeInt = 2;
         }
         if (Input.GetKeyDown(KeyCode.Alpha4))
         {
             controlMode = ControlMode.obstacle;
-            controlModeInt = 3;
         }
-    }
 
+    }
+    private void Awake()
+    {
+        //GetComponent<Renderer>().material.SetColor("_BaseColor", Random.ColorHSV(0, 1, 0.5f, 1, 0.5f, 1));
+    }
 }

d3d014137d048104f1cc1d61a07824b3c97a4a91
TomascrazyDX <tomasarango22@gmail.com>
2025-05-14 09:55:52 -0400

    fixed the color issue and erased  the rigid body not needed for now

--- Assets/Boid.cs
+++ Assets/Boid.cs
@@ -3,18 +3,8 @@ using UnityEngine.UIElements;
 
 public class Boid : MonoBehaviour
 {
-    public Rigidbody rigidbody;
     private void Awake()
     {
-        rigidbody = GetComponent<Rigidbody>();
-        rigidbody.linearVelocity = Random.insideUnitSphere;
-    }
-    public void Update()
-    {
-        AlignToVelocity();
-    }
-    public void AlignToVelocity()
-    {
-    //transform.forward = new Vector3.RotateTowards(transform.forward, rigidbody.linearVelocity.normalized, Mathf.Deg2Rad * 1800 *  Time.deltaTime * 100);
+        GetComponent<Renderer>().material.SetColor("_BaseColor", Random.ColorHSV(0, 1, 0.5f, 1, 0.5f, 1));
     }
 }
--- Assets/BoidSimulation.cs
+++ Assets/BoidSimulation.cs
@@ -57,8 +57,5 @@ public class BoidSimulationControl : MonoBehaviour
         }
 
     }
-    private void Awake()
-    {
-        //GetComponent<Renderer>().material.SetColor("_BaseColor", Random.ColorHSV(0, 1, 0.5f, 1, 0.5f, 1));
-    }
+
 }

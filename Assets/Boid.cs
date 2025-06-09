10c8157439dc096d597a31664341ab8eb046df90
TomascrazyDX <tomasarango22@gmail.com>
2025-06-08 22:28:02 -0400

    revisited the codes to get them ready for Assignment 1

--- Assets/Boid.cs
+++ Assets/Boid.cs
@@ -3,8 +3,46 @@ using UnityEngine.UIElements;
 
 public class Boid : MonoBehaviour
 {
+    
+    public Rigidbody rigidBody;
+    public float speedMax = 2;
+    public float accelMax = 3;
+
+    private void Start()
+    {
+     
+       rigidBody = GetComponent<Rigidbody>();
+    }
     private void Awake()
     {
         GetComponent<Renderer>().material.SetColor("_BaseColor", Random.ColorHSV(0, 1, 0.5f, 1, 0.5f, 1));
     }
+
+    private void Update()
+    {
+      Debug.DrawRay(transform.position, rigidBody.linearVelocity, Color.red);
+
+    }
+    private void FixedUpdate()
+    {
+        //oeiwnt toward velocity
+        transform.forward = rigidBody.linearVelocity;
+    }
+    public Vector3 Seek(Vector3 target, float acceleration)
+    {
+        //get  displacement vector to target
+        Vector3 toTarget  = target  - transform.position;
+
+        //Normalize ti get the direction to the target
+        Vector3 toTargetNormalized = toTarget.normalized;
+
+        //ddetermine the acceleration  with given magnitude
+        Vector3 accel = toTargetNormalized * acceleration;
+
+        return accel;
+    }
+
+
+
+
 }
--- Assets/BoidSimulation.cs
+++ Assets/BoidSimulation.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class BoidSimulationControl : MonoBehaviour
 {
+    public GameObject targetObject;
     public GameObject boidPrefab = null;
     public int BoidsToSpawn = 10;
 
@@ -23,22 +24,28 @@ public class BoidSimulationControl : MonoBehaviour
 
     private void Start()
     {
-        Vector3 position = new Vector3(Random.Range(-1.4f, 1.4f), Random.Range(0f, 1.4f), Random.Range(-0.9f, 0.9f));
+        targetObject = GameObject.Find("target");
+
         Quaternion rotation = Random.rotation;
+        Vector3 position = new Vector3(Random.Range(-1.4f, 1.4f), Random.Range(0f, 1.4f), Random.Range(-0.9f, 0.9f));
+
+        //spawn the boids
 
         for (int i = 0; i <= BoidsToSpawn; i++)
         {
-            GameObject newBoid = Instantiate(boidPrefab, new Vector3(Random.Range(-0.7f, 0.7f), Random.Range(0, 0.7f), Random.Range(-0.4f, 0.4f)), Random.rotation);
-            boids.Add(newBoid.GetComponent<Boid>());
-            newBoid.GetComponent<Rigidbody>().linearVelocity = Random.onUnitSphere * 0.3f;
-            
-            newBoid.transform.localScale *= Random.Range(0.9f, 3f);
+            GameObject spawnedBoid = Instantiate(boidPrefab, position, rotation);
+
+            Boid boidComponent = spawnedBoid.GetComponent<Boid>();
+            //Randomize  properties of each boid within range
+            boidComponent.speedMax = Random.Range(1f, 3f);
+            boidComponent.accelMax = Random.Range(1f, 5f);
+
+            boids.Add(boidComponent); //add the new object to the list so we can use later
+
+            Vector3 accel = boids[i].Seek(targetObject.transform.position, boids[i].accelMax);
+            boids[i].rigidBody.linearVelocity += accel * Time.fixedDeltaTime; //apply the acceleration
+            Debug.DrawRay(boids[i].transform.position, accel, Color.green);//draw acceleration
         }
-
-        //spawn the boids
-        GameObject spawnedBoid = Instantiate(boidPrefab, position, rotation);
-
-        spawnedBoid.transform.localScale *= Random.Range(0.9f, 3f);
     }
     private void Update()
     {
@@ -58,7 +65,18 @@ public class BoidSimulationControl : MonoBehaviour
         {
             controlMode = ControlMode.obstacle;
         }
-
     }
+    private void FixedUpdate()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hitInfo;
+        bool didHit = Physics.Raycast(ray, out hitInfo, 100);
+
+        if (didHit)
+        {
+            targetObject.transform.position = hitInfo.point; //move the target to the mouse position
+        }
+    }
+
 
 }
